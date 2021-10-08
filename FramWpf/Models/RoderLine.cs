189db163317f9e557f494e647e9500b794e46463
GylkaTimophey <56362432+GylkaTimophey@users.noreply.github.com>
2021-10-08 12:29:50 +0300

    Добавьте файлы проекта.

---
+++ FramWpf/Models/RoderLine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Shapes;
+using Prism.Mvvm;
+using Prism.Commands;
+using System.ComponentModel;
+
+namespace RoderCADUI.Model {
+    internal class RoderLine : BindableBase {
+
+        private event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        static int count = 0;
+        static int Id;
+        internal RoderLine() {
+            Id = count++;
+        }
+        internal RoderLine(Point startPoint, Point endPoint, Brush brush) {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            Brush = brush;
+            Id = count++;
+        }
+
+        private Point startPoint;
+        internal Point StartPoint {
+            get {
+                return startPoint;
+            }
+            set {
+                startPoint = value;
+                OnPropertyChanged("roderLine");
+            }
+        }
+        private Point endPoint;
+        internal Point EndPoint {
+            get {
+                return endPoint;
+            }
+            set {
+                endPoint = value;
+                OnPropertyChanged("roderLine");
+            }
+        }
+        private Brush brush;
+        internal Brush Brush {
+            get {
+                return brush;
+            }
+            set {
+                brush = value;
+                OnPropertyChanged("roderLine");
+            }
+        }
+    }
+}
---
+++ FramWpf/Models/RoderLineList.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using RoderCADUI.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace FramWpf.Models {
+    class RoderLineList : BindableBase {
+        internal ObservableCollection<RoderLine> lineList;
+        public RoderLineList() {
+            lineList = new ObservableCollection<RoderLine>();
+            lineList.Add(new RoderLine(new Point(14, 67), new Point(12,67), Brushes.DarkGreen));
+            lineList.Add(new RoderLine(new Point(23, 78), new Point(23, 78), Brushes.DarkGreen));
+            lineList.Add(new RoderLine(new Point(34, 89), new Point(34, 89), Brushes.DarkGreen));
+            lineList.Add(new RoderLine(new Point(45, 90), new Point(45, 90), Brushes.DarkGreen));
+            lineList.Add(new RoderLine(new Point(56, 123), new Point(56, 123), Brushes.DarkGreen));
+        }
+    }
+}
